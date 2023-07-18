9cd38426d58590e49bd00f9ae833103e2d7c9417
Veronika Ihnatova <veronika.a.ignatova@gmail.com>
2023-07-18 13:34:13 +0300

    Add project files.

---
+++ FindTheLetter/Program.cs
@@ -0,0 +1,49 @@
+﻿char[] divider = { ' ', '\t', '\n', '\r', '.', ',', '-', '"' };
+
+List<char> firstLetters = new List<char> ();
+
+string textFile = "text.txt";
+string text = File.ReadAllText(textFile);
+
+string[] words = text.Split(divider, StringSplitOptions.RemoveEmptyEntries);
+
+ char FirstUniqueLetter(string word)
+{
+    Dictionary<char, int> numberOfLetters = new Dictionary<char, int>();
+
+    foreach (char letter in word)
+    {
+        if (numberOfLetters.ContainsKey(letter))
+        {
+            numberOfLetters[letter]++;
+        }
+        else
+        {
+            numberOfLetters[letter] = 1;
+        }
+    }
+
+    foreach (char letter in word)
+    {
+        if (numberOfLetters[letter] == 1)
+        {
+            return letter;
+        }
+    }
+
+    return word[word.Length];
+}
+
+for (int i = 0; i < words.Length; i++)
+{
+    firstLetters.Add(FirstUniqueLetter(words[i]));
+}
+
+Console.WriteLine("This is your text: " + text);
+
+char result = FirstUniqueLetter(string.Join("", firstLetters));
+Console.WriteLine("This is your letter: " + result);
+
+
+//Console.WriteLine("{0}", string.Join(", ", firstLetters));
+//Console.WriteLine("The searched letter is: " + "{0}", string.Join(", ", words));
